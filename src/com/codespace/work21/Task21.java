82dba59651bb31818a055c2730380344473dca40
welesmir <welesmir@gmail.com>
2022-02-02 01:11:04 +0200

    Initial commit

---
+++ src/com/codespace/work21/Task21.java
@@ -0,0 +1,8 @@
+package com.codespace.work21;
+
+public class Task21 {
+    public static void main(String[] args) {
+        System.out.println("секунд в 3 неделях: " + (3*7*24*60*60));
+    }
+
+}
